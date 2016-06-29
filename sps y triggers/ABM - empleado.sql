c22080c55102f4c09049c3e565c4d624afbfb5ea
LenaCM <male.corrales.m@gmail.com>
2016-06-29 00:54:20 -0300

    Modificacion empleado y algo mas
    Algunas modificaciones

--- sps y triggers/ABM - empleado.sql
+++ sps y triggers/ABM - empleado.sql
@@ -1,5 +1,6 @@
 ﻿--ABM empleados
 --alta empleado
+/*
 create or replace function sp_alta_empleado(nombre_ text, apellido_ text, doc integer, tip_doc text, sueldo numeric, ant integer, ho_ent time without time zone, ho_sal time without time zone)
 	returns void as
 $$
@@ -17,5 +18,52 @@ begin
 end;
 $$
 	language plpgsql;
-	
+*/
 --select sp_alta_empleado('Esteban', 'Quito', 33333333, 'LE',8999.99, 5, '08:00:00', '12:00:00')
+
+--modificacion empleado
+/*
+create or replace function sp_modificacion_empleado(doc integer, tipo_d text, que integer, dato text)
+	returns void as
+$$
+declare 
+	id_pers integer;
+begin
+	if dato is null then
+		raise exception 'Dato inválido';
+	end if;
+	id_pers := (select id_persona from empleados e inner join personas p using(id_persona)  where p.dni=doc and p.id_tipo_doc=(select id_tipo_doc from tipos_doc where tipo_doc like '%'||tipo_d||'%'));
+	if id_pers is not null then
+		if que=1 or que=2 or que=3 or que=4 then
+		
+			perform sp_modificacion_persona(doc, tipo_d, que, dato);
+			
+		elsif que=5 then
+		
+			update empleados
+			set salario=cast(dato as numeric)
+			where id_persona=id_pers;
+			
+		elsif que=6 then
+		
+			update empleados
+			set antiguedad=cast(dato as int4)
+			where id_persona=id_pers;
+			
+		elsif que=7 then
+			update empleados
+			set hora_entrada=cast(dato as time)
+			where id_persona=id_pers;
+		elsif que=8 then
+			update empleados
+			set hora_salida=cast(dato as time)
+			where id_persona=id_pers;
+		end if;
+	else
+		raise exception 'El emplead@ no existe';
+	end if;
+end;
+$$
+	language plpgsql;
+*/
+--select sp_modificacion_empleado(33333334, 'LE', 8, '13:00:00')
--- sps y triggers/ABM - persona.sql
+++ sps y triggers/ABM - persona.sql
@@ -48,22 +48,26 @@ begin
 	if dato is null then
 		raise exception 'Dato inválido';
 	end if;
-	if que=1 then
-		update personas
-			set nombre=dato
-		where dni=doc and id_tipo_doc=(select id_tipo_doc from tipos_doc where tipo_doc like '%'||tipo_d||'%');
-	elsif que=2 then
-		update personas
-			set apellido=dato
-		where dni=doc and id_tipo_doc=(select id_tipo_doc from tipos_doc where tipo_doc like '%'||tipo_d||'%');
-	elsif que=3 then
-		update personas
-			set dni=cast(dato as int4)
-		where dni=doc and id_tipo_doc=(select id_tipo_doc from tipos_doc where tipo_doc like '%'||tipo_d||'%');
-	elsif que=4 then
-		update personas
-			set id_tipo_doc=(select id_tipo_doc from tipos_doc where tipo_doc like '%'||dato||'%')
-		where dni=doc and id_tipo_doc=(select id_tipo_doc from tipos_doc where tipo_doc like '%'||tipo_d||'%');
+	if exists(select * from personas where dni=doc and id_tipo_doc=(select id_tipo_doc from tipos_doc where tipo_doc like '%'||tipo_d||'%')) then
+		if que=1 then
+			update personas
+				set nombre=dato
+			where dni=doc and id_tipo_doc=(select id_tipo_doc from tipos_doc where tipo_doc like '%'||tipo_d||'%');
+		elsif que=2 then
+			update personas
+				set apellido=dato
+			where dni=doc and id_tipo_doc=(select id_tipo_doc from tipos_doc where tipo_doc like '%'||tipo_d||'%');
+		elsif que=3 then
+			update personas
+				set dni=cast(dato as int4)
+			where dni=doc and id_tipo_doc=(select id_tipo_doc from tipos_doc where tipo_doc like '%'||tipo_d||'%');
+		elsif que=4 then
+			update personas
+				set id_tipo_doc=(select id_tipo_doc from tipos_doc where tipo_doc like '%'||dato||'%')
+			where dni=doc and id_tipo_doc=(select id_tipo_doc from tipos_doc where tipo_doc like '%'||tipo_d||'%');
+		end if;
+	else 
+		raise exception 'La persona no existe';
 	end if;
 	exception
 		when unique_violation then
