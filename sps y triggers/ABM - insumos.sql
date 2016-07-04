14011b8f86a22b1c1c0ce74d12b4ad9388e55512
noeAL16 <noe61800@gmail.com>
2016-07-04 16:53:23 -0300

    pruebas

---
+++ sps y triggers/ABM - insumos.sql
@@ -0,0 +1,83 @@
+﻿--ABM Insumos:
+
+--Alta
+create or replace function sp_alta_insumos(nombre_ text, stock_ integer)
+	returns void as $$
+declare
+	id_ins integer;
+begin
+	--controles
+	if nombre_='' then
+		raise exception 'El campo nombre no puede estar vacio';
+	end if;
+	if stock_ < 0 then
+		raise exception 'El stock no puede ser negativo';
+	end if;
+	--determinar el id
+	if (select max(id_insumo) from insumos) is null then
+		id_ins:=1;
+	else
+		id_ins:= (select max(id_insumo) from insumos) + 1;
+	end if;
+	--control de existencia del proveedor
+	if (select id_insumo from insumos where nombre like '%' || nombre_ || '%') is null then
+		INSERT INTO insumos(id_insumo, nombre, stock)
+			VALUES (id_ins, nombre_, stock_);
+	else
+		raise exception 'El Insumo ya existe';
+	end if;
+end;
+$$language plpgsql;
+
+
+--prueba para dar de alta:
+select sp_alta_insumos('Escobas', 3);
+
+-----------------------------------------------------------------------------------------------------------------
+
+--modificar 
+
+create or replace function sp_modificacion_insumos(idins integer, que integer, dato text)
+	returns void as
+$$
+begin
+	if dato is null then
+		raise exception 'Dato inválido';
+	end if;
+	if exists(select * from insumos where id_insumo=idins) then
+		if que=1 then
+			update insumos
+				set nombre=dato
+			where id_insumo=idins;
+		elsif que=2 then
+			update insumos
+				set stock=cast(dato as int4)
+			where id_insumo=idins;
+		end if;
+	else 
+		raise exception 'El insumo no existe';
+	end if;
+end;
+$$language plpgsql;
+
+--prueba
+select sp_modificacion_insumos(2, 2, '32');
+
+-----------------------------------------------------------------------------------------------------------------
+--Baja
+create or replace function sp_baja_insumos(idins integer)
+	returns void as
+$$
+declare
+begin
+	if (select id_insumo from insumos where id_insumo=idins) is null then
+		raise exception 'El insumo no existe';
+	else
+		delete from insumos
+		where id_insumo=idins;
+	end if;
+end;
+$$
+language plpgsql;
+
+select sp_baja_insumos(2)
---
+++ sps y triggers/ABM - proveedores.sql
@@ -0,0 +1,99 @@
+﻿--ABM Proveedores:
+
+--Alta
+create or replace function sp_alta_proveedores(nombre_ text, dir text, tel text)
+	returns void as $$
+declare
+	id_prov integer;
+begin
+	--controles
+	if nombre_='' then
+		raise exception 'El campo nombre no puede estar vacio';
+	end if;
+	if dir='' then
+		raise exception 'El campo direccion no puede estar vacio';
+	end if;
+	if tel='' then
+		raise exception 'El campo telefono no puede estar vacio';
+	end if;
+	--determinar el id
+	if (select max(id_proveedor) from proveedores) is null then
+		id_prov:=1;
+	else
+		id_prov:= (select max(id_proveedor) from proveedores) + 1;
+	end if;
+	--control de existencia del proveedor
+	if (select id_persona from personas where dni=doc and id_tipo_doc=id_doc) is null then
+		--determinar el id de persona
+		if (select max(id_persona) from personas) is null then
+			id_pers:=1;
+		else
+			id_pers:= (select max(id_persona) from personas) + 1;
+		end if;
+		--insertar persona
+		perform sp_alta_persona(nombre_, apellido_, doc, tip_doc);
+	else
+		id_pers:=(select id_persona from personas where dni=doc and id_tipo_doc=id_doc);
+	end if;
+	--insertar No_socio
+	insert into no_socios(id_persona)
+   		values (id_pers);
+   	exception			
+		when unique_violation then
+			raise exception 'El documento ya existe';
+end;
+$$language plpgsql;
+
+
+--prueba para dar de alta no_socio:
+select sp_alta_no_socio('Ana', 'Jimenez', 11234567, 'DNI')
+
+-----------------------------------------------------------------------------------------------------------------
+
+--modificar no socio
+
+create or replace function sp_modificacion_no_socio(tipo_busq integer, tipo_d text, numero integer, campo integer, dato text)
+	returns void as
+$$
+declare docu integer; tipo_dc text;
+begin
+	if tipo_busq=1 then
+		docu:=(select dni from personas p inner join no_socios s using(id_persona) where s.id_persona=numero);
+		tipo_dc:=(select tipo_doc from tipos_doc t inner join personas p using(id_tipo_doc) where p.dni=docu);
+		perform sp_modificacion_persona(docu,tipo_dc, campo, dato); 
+	elsif tipo_busq=2 then
+		perform sp_modificacion_persona(numero, tipo_d, campo, dato);
+	end if;	
+end;
+$$
+language plpgsql;
+
+--select sp_modificacion_socio(1,'DNI',3,2,'Saenz')
+--select sp_modificacion_persona(13234567, 'LC', 2, 'Saenz')
+
+-----------------------------------------------------------------------------------------------------------------
+--Baja
+create or replace function sp_borrar_no_socio(doc integer, tip_doc text)
+	returns void as $$
+declare
+	id_pers integer; id_doc smallint;
+begin
+	--controles
+	if doc<0 then
+		raise exception 'El campo documento no puede ser negativo o estar vacio';
+	end if;
+	
+	--determinar id
+	id_doc:= (select id_tipo_doc from tipos_doc where tipo_doc like '%'||tip_doc||'%');
+	id_pers:=(select id_persona from personas where dni=doc and id_tipo_doc=id_doc);
+	--control de existencia de persona
+	if (select id_persona from personas where dni=doc and id_tipo_doc=id_doc) is null then
+		raise exception 'La persona que busca no se encuentra en BD';
+	else
+		perform sp_baja_persona(doc,tip_doc);
+	end if;
+end;
+$$language plpgsql;
+
+--prueba para eliminar socio:
+select sp_borrar_no_socio(11234567, 'DNI');
