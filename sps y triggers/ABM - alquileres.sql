6695e8a40818f90b933c6bace8e1d0b1076b07b9
LenaCM <male.corrales.m@gmail.com>
2016-07-07 05:41:42 -0300

    alta alquiler de socios y no socios
    agregue el campo pagado en ambas tablas ´porque no lo tenian. modifique
    el sp de listado para que pueda mostrar el pagado

---
+++ sps y triggers/ABM - alquileres.sql
@@ -0,0 +1,100 @@
+﻿--ABM alquileres
+-----------------------------------------------------------------------------------------------------
+--ALTA alquiler socio
+-----------------------------------------------------------------------------------------------------
+/*
+create or replace function sp_alta_alquiler(tipo_busq integer, tipo_d varchar, numero integer, instal varchar, fecha_al date, hora_al time without time zone,costo_al numeric, pagado_al boolean)
+	returns void as
+$$
+	declare num_soc integer; id_pers integer; id_inst smallint;
+begin
+	if instal='' or instal is null then
+		raise exception 'Instalación no válida';
+	end if;
+	if fecha_al<current_date then
+		raise exception 'La fecha no puede ser anterior a la fecha actual';
+	end if;
+	if costo_al<=0 then
+		raise exception 'El monto ingresado no es valido';
+	end if;
+	if hora_al<'07:00:00' or hora_al>'22:00:00' then
+		raise exception 'Alquiler fuera de horario';
+	end if;
+	id_inst := (select id_instalacion from instalaciones where nombre_instalacion like '%'||instal||'%');
+	if (select id_instalacion from socios_alquilan where id_instalacion=id_inst and fecha=fecha_al and hora=hora_al) is not null then
+		raise exception 'La instalacion ya se encuentra alquilada en este turno';
+	end if;
+	
+	if tipo_busq=1 then
+		if (select numero_socio from socios where numero_socio=numero) is null then
+			raise exception 'El socio no existe';
+		elsif (select numero_socio from socios_activos where numero_socio=numero) is null then
+			raise exception 'El socio no esta activo';
+		else
+			num_soc:=numero;
+			id_pers = (select id_persona from socios where numero_socio=num_soc);
+		end if;
+	else
+		num_soc := (select id_persona from socios inner join personas using(id_persona) where dni=numero and id_tipo_doc=(select busca_id_documento(tipo_d)));
+		if num_soc is null then
+			raise exception 'El socio no existe';
+		elsif (select num_soc from socios_activos where numero_socio=num_soc) is null then
+			raise exception 'El socio no esta activo';
+		else
+			id_pers = (select id_persona from socios where numero_socio=num_soc);
+		end if;
+	end if;
+	insert into socios_alquilan values (num_soc, id_inst, fecha_al, hora_al, costo_al,id_pers, pagado_al);
+	exception 
+		when unique_violation then
+			raise exception 'Ya se registro el alquiler';
+			
+	
+end;
+$$
+language plpgsql;
+*/
+--select sp_alta_alquiler(2, 'LC', 39567455, 'PISCINA 1', '09/07/2016', '07:00:00', 234.45,false)
+-----------------------------------------------------------------------------------------------------
+--ALTA alquiler no socio
+-----------------------------------------------------------------------------------------------------
+/*
+create or replace function sp_alta_alquiler(tipo_d varchar, numero integer, nombre_al varchar, apellido_al varchar, instal varchar, fecha_al date, hora_al time without time zone,costo_al numeric, senia_al numeric, pagado_al boolean)
+	returns void as
+$$
+declare
+	id_inst smallint; id_pers integer;
+begin
+	if instal='' or instal is null then
+		raise exception 'Instalación no válida';
+	end if;
+	if fecha_al<current_date then
+		raise exception 'La fecha no puede ser anterior a la fecha actual';
+	end if;
+	if costo_al<=0 then
+		raise exception 'El monto de costo alquiler ingresado no es valido';
+	end if;
+	if senia_al<=0 then
+		raise exception 'El monto ingresado como senia no es valido';
+	end if;
+	if hora_al<'07:00:00' or hora_al>'22:00:00' then
+		raise exception 'Alquiler fuera de horario';
+	end if;
+	id_inst := (select id_instalacion from instalaciones where nombre_instalacion like '%'||instal||'%');
+	if (select id_instalacion from no_socios_alquilan where id_instalacion=id_inst and fecha=fecha_al and hora=hora_al) is not null then
+		raise exception 'La instalacion ya se encuentra alquilada en este turno';
+	end if;
+	if (select id_persona from personas where dni=numero and id_tipo_doc=(select busca_id_documento(tipo_d))) is null then
+		perform sp_alta_persona(nombre_al, apellido_al, numero, tipo_d);
+		insert into no_socios values (id_pers);
+		id_pers := (select id_persona from personas where dni=numero and id_tipo_doc=(select busca_id_documento(tipo_d)));
+	else
+		id_pers := (select id_persona from personas where dni=numero and id_tipo_doc=(select busca_id_documento(tipo_d)));
+	end if;
+	
+	insert into no_socios_alquilan values(id_pers, id_inst, fecha_al, hora_al, costo_al, senia_al, pagado_al);
+end;
+$$
+language plpgsql;
+*/
+-- select sp_alta_alquiler('LE', 16509233, 'Demetria' , 'Fisher', 'CANCHA DE FUTBOL 5 1', '20/07/2016', '12:00:00', 532.99, 100.00, false);
--- sps y triggers/Listado-alquiler.sql
+++ sps y triggers/Listado-alquiler.sql
@@ -1,13 +1,14 @@
 ﻿--Listado alquileres:
-
+/*******************************************************************************
 create type listado_alquileres as (apellido character varying(32),
 				nombre character varying(32),
 				nombre_instalacion character varying(32),
 				fecha date,
 				hora time,
 				costo numeric(6,2),
-				numero_socio integer);
-
+				numero_socio integer,
+				pagado boolean);
+**********************************************************************************/
 				
 CREATE OR REPLACE FUNCTION sp_listado_alquiler()
 RETURNS SETOF listado_alquileres AS $$
@@ -16,7 +17,7 @@ rec listado_alquileres%rowtype;
 
 BEGIN
 	
-for rec in select apellido, nombre, nombre_instalacion, fecha, hora, costo, numero_socio
+for rec in select apellido, nombre, nombre_instalacion, fecha, hora, costo, numero_socio, pagado
 		from socios_alquilan  
 		inner join personas using(id_persona)
 		inner join instalaciones i using(id_instalacion)
@@ -25,7 +26,7 @@ for rec in select apellido, nombre, nombre_instalacion, fecha, hora, costo, nume
 		return next rec;
 	end loop;
 	
-for rec in select apellido, nombre, nombre_instalacion, fecha, hora, costo
+for rec in select apellido, nombre, nombre_instalacion, fecha, hora, costo, 0, pagado
 		from no_socios_alquilan  
 		inner join personas using(id_persona)
 		inner join instalaciones i using(id_instalacion)
@@ -35,8 +36,7 @@ for rec in select apellido, nombre, nombre_instalacion, fecha, hora, costo
 	end loop;
 
 END;
-$$LANGUAGE plpgsql VOLATILE
-COST 100;
+$$LANGUAGE plpgsql;
 
 /*pruebas
-SELECT * from sp_listado_alquiler(); 
+SELECT * from sp_listado_alquiler(); */
