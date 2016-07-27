d4faa170ce9418059a6ae11491987771d3879abc
LenaCM <male.corrales.m@gmail.com>
2016-07-27 18:50:58 -0300

    seccion empleados

--- sps y triggers/Listado-alquiler.sql
+++ sps y triggers/Listado-alquiler.sql
@@ -17,26 +17,53 @@ rec listado_alquileres%rowtype;
 
 BEGIN
 	
-for rec in select apellido, nombre, nombre_instalacion, fecha, hora, costo, numero_socio, pagado
-		from socios_alquilan  
-		inner join personas using(id_persona)
-		inner join instalaciones i using(id_instalacion)
-		order by apellido
+for rec in select * from 
+			((select apellido, nombre, nombre_instalacion, fecha, hora, costo, numero_socio, pagado 
+			from socios_alquilan 
+			inner join personas using(id_persona)
+			inner join instalaciones using(id_instalacion) order by hora desc)
+			union 
+			(select apellido, nombre, nombre_instalacion, fecha, hora, costo, 0, pagado 
+			from no_socios_alquilan 
+			inner join personas using(id_persona)
+			inner join instalaciones using(id_instalacion)order by hora desc))as alquileres
+		order by alquileres.fecha desc
 	loop
 		return next rec;
 	end loop;
 	
-for rec in select apellido, nombre, nombre_instalacion, fecha, hora, costo, 0, pagado
-		from no_socios_alquilan  
-		inner join personas using(id_persona)
-		inner join instalaciones i using(id_instalacion)
-		order by apellido
-	loop
-		return next rec;
-	end loop;
 
 END;
 $$LANGUAGE plpgsql;
 
 /*pruebas
 SELECT * from sp_listado_alquiler(); */
+
+CREATE OR REPLACE FUNCTION sp_listado_alquiler(apellido_ text)
+RETURNS SETOF listado_alquileres AS $$
+DECLARE
+rec listado_alquileres%rowtype; 
+
+BEGIN
+	
+for rec in select * from 
+			((select apellido, nombre, nombre_instalacion, fecha, hora, costo, numero_socio, pagado 
+			from socios_alquilan 
+			inner join personas using(id_persona)
+			inner join instalaciones using(id_instalacion) order by hora desc)
+			union 
+			(select apellido, nombre, nombre_instalacion, fecha, hora, costo, 0, pagado 
+			from no_socios_alquilan 
+			inner join personas using(id_persona)
+			inner join instalaciones using(id_instalacion)order by hora desc))as alquileres
+		where apellido like '%'||apellido_||'%'
+		order by alquileres.fecha desc
+	loop
+		return next rec;
+	end loop;
+	
+
+END;
+$$LANGUAGE plpgsql;
+
+select * from sp_listado_alquiler('MEN')
